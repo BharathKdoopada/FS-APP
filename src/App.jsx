549779edcdac93f7a74a63556795f9d70e3690e8
BharathKdoopada <bharathdoopada@gmail.com>
2025-06-29 02:03:37 +0530

    ui developed

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,29 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './Login'
-import Home from './Home'
+import { useState } from "react";
+import "./App.css";
+import Login from "./Login";
+import Home from "./Home";
+import Mainpage from "./Pages/mainpage";
+import Reading from "./Pages/reading";
+import Report from "./Pages/reports";
+import Billing from "./Pages/billing";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
 function App() {
-
   return (
     <BrowserRouter>
-  <div className="h-[100dvh] w-full">
-    <Routes>
-    <Route
-        path="/"
-        element={<Home/>} />
-      <Route
-        path="/login"
-        element={
-          <div
-            className="w-full h-full grid justify-center items-center"
-            style={{ backgroundImage: "linear-gradient(to right, #DECBA499, #3E515199)" }}
-          >
-            <Login />
-          </div>
-        }
-      />
-    </Routes>
-  </div>
-</BrowserRouter>
-
-  )
+      <div className="h-[100dvh] w-full">
+        <Routes>
+          <Route path="/dashboard" element={<Home />}>
+            <Route index element={<Mainpage />} />
+            <Route path="billing" element={<Billing />} />
+            <Route path="reports" element={<Report />} />
+            <Route path="readings" element={<Reading />} />
+          </Route>
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,69 +1,66 @@
-import React from 'react'
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import LoginImg from "./assets/login_img.jpg"
-import { Button } from "@/components/ui/button"
-import { Card, CardContent } from "@/components/ui/card"
-
+import React from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import LoginImg from "./assets/login_img.jpg";
+import { Button } from "@/components/ui/button";
+import { Card, CardContent } from "@/components/ui/card";
 
 export default function Login() {
   return (
-    <div className="flex flex-col gap-6">
-      <Card className="overflow-hidden w-[300px] md:w-[400px] mx-auto bg-white/10 backdrop-blur-2xl rounded-xl border-0 p-6 sm:p-2 shadow-lg">
-        <CardContent className="grid p-0">
-          <form className="p-6 md:p-8 sm:p-2">
-            <div className="flex flex-col gap-6">
-              <div className="flex flex-col items-center text-center">
-                <h1 className="text-2xl font-bold">Welcome back</h1>
-                <p className=" text-balance text-gray-900 dark:text-gray-400">
-                  Login to your FS account
-                </p>
-              </div>
-              <div className="grid gap-3">
-                <Label  htmlFor="email">Email</Label>
-                <Input
-                  id="email"
-                  type="email"
-                  required
-                />
-              </div>
-              <div className="grid gap-3">
-                <div className="flex items-center">
-                  <Label htmlFor="password">Password</Label>
-                  <a
-                    href="#"
-                    className="ml-auto text-sm underline-offset-2 hover:underline"
-                  >
-                    Forgot your password?
+    <div className="w-full h-full grid justify-center items-center bg-[#fffbeb]">
+      <div className="flex flex-col gap-6">
+        <Card className="overflow-hidden w-[300px] md:w-[400px] mx-auto bg-white backdrop-blur-2xl border-4 border-black p-5 sm:p-6 shadow-[6px_6px_0_0_rgba(0,0,0,1)] rounded-xl font-mono text-black">
+          <CardContent className="grid p-0">
+            <form className="p-6 md:p-8 sm:p-2">
+              <div className="flex flex-col gap-6">
+                <div className="flex flex-col items-center text-center">
+                  <h1 className="text-2xl font-bold">Welcome back</h1>
+                  <p className=" text-balance text-black dark:text-gray-400">
+                    Login to your FS account
+                  </p>
+                </div>
+                <div className="grid gap-3">
+                  <Label htmlFor="email">Email</Label>
+                  <Input id="email" type="email" required />
+                </div>
+                <div className="grid gap-3">
+                  <div className="flex items-center">
+                    <Label htmlFor="password">Password</Label>
+                    <a
+                      href="#"
+                      className="ml-auto text-sm underline-offset-2 hover:underline"
+                    >
+                      Forgot your password?
+                    </a>
+                  </div>
+                  <Input id="password" type="password" required />
+                </div>
+                <Button type="submit" className="w-full">
+                  Login
+                </Button>
+
+                <div className="text-center text-sm">
+                  Don&apos;t have an account?{" "}
+                  <a href="#" className="underline underline-offset-4">
+                    Register
                   </a>
                 </div>
-                <Input id="password" type="password" required />
               </div>
-              <Button type="submit" className="w-full">
-                Login
-              </Button>
-             
-              <div className="text-center text-sm">
-                Don&apos;t have an account?{" "}
-                <a href="#" className="underline underline-offset-4">
-                  Sign up
-                </a>
-              </div>
-            </div>
-          </form>
-          {/* <div className="bg-muted relative hidden md:block">
+            </form>
+            {/* <div className="bg-muted relative hidden md:block">
             <img
               src={LoginImg}
               alt="Image"
               className="absolute bottom-0  h-full w-full object-cover  dark:brightness-[0.2] dark:grayscale"
             />
           </div> */}
-        </CardContent>
-      </Card>
-      {/* <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
+          </CardContent>
+        </Card>
+        {/* <div className="text-muted-foreground *:[a]:hover:text-primary text-center text-xs text-balance *:[a]:underline *:[a]:underline-offset-4">
         By clicking continue, you agree to our <a href="#">Terms of Service</a>{" "}
         and <a href="#">Privacy Policy</a>.
       </div> */}
+      </div>
     </div>
-  )
+  );
 }
